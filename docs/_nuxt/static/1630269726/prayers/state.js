5174a964a4c9caab194a6c2775335ecc14706333
Isaac Smith <isaac.smith.sf@gmail.com>
2021-08-29 15:53:41 -0500

    Release 2021082902

--- docs/_nuxt/static/1630269726/prayers/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsite\u002F_nuxt\u002Fstatic\u002F1630269726",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprayers",config:{_app:{basePath:"\u002Fsite\u002F",assetsPath:"\u002Fsite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
