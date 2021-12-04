13c5b137271af1c055e43880d0c5fc7c2bcda4c7
Isaac Smith <isaac.smith.sf@gmail.com>
2021-12-04 17:59:18 -0600

    Finished mass cards image layout

--- src/site/pages/mass-cards.vue
+++ src/site/pages/mass-cards.vue
@@ -1,30 +1,19 @@
 <template>
   <div class="pb-10">
-    <img
-      src="~/assets/mass-cards/Monstrance_Feature.jpg"
-      alt="Monstrance on an altar"
-    />
+    <img src="~/assets/mass-cards/Monstrance_Feature.jpg" alt="Monstrance on an altar" />
     <div class="my-5 grid grid-flow-col px-2">
-      <div
-        class="col-span-full md:col-span-4 flex flex-col m-auto text-center md:mt-0"
-        :class="{ 'mt-20': i > 0 }"
-      >
-        <img
-          src="~/assets/mass-cards/Cross_Icon.png"
-          alt="Cross"
-          width="150"
-          class="mb-5"
-        />
-        <h2 class="text-primary text-xl font-semibold">
-          Deceased Loved Ones
-        </h2>
-        <nuxt-link to="https://secure.acceptiva.com/?cst=rEzKFK">
-          <div
-            class="p-2 bg-primary text-white mt-7 text-lg rounded mb-8 mx-10"
-          >
-            Request Card
-          </div>
-        </nuxt-link>
+      <div class="col-span-full md:col-span-4 flex flex-col m-auto text-center md:mt-0 mt-20">
+        <div class="grid place-items-center">
+          <img src="~/assets/mass-cards/Cross_Icon.png" alt="Cross" width="150" class="mb-5" />
+        </div>
+        <h2 class="text-primary text-xl font-semibold">Deceased Loved Ones</h2>
+        <a
+          href="https://secure.acceptiva.com/?cst=rEzKFK"
+          target="_blank"
+          rel="nofollow noreferrer"
+        >
+          <div class="p-2 bg-primary text-white mt-7 text-lg rounded mb-8 mx-10">Request Card</div>
+        </a>
         <p class="text-left">
           The loss of a loved one is difficult. Help honor the memory of those
           you knew and loved by requesting a special Mass intention, at Jesus,
@@ -34,24 +23,23 @@
       </div>
       <div
         class="col-span-full md:col-span-4 flex flex-col m-auto text-center md:mt-0 mx-0 md:mx-8"
-        :class="{ 'mt-20': i > 0 }"
       >
-        <img
-          src="~/assets/mass-cards/Ring_Cross_Icon-225x300.png"
-          alt="Cross with wedding rings"
-          width="150"
-          class="mb-5"
-        />
-        <h2 class="text-primary text-xl font-semibold">
-          Wedding Anniversaries
-        </h2>
-        <nuxt-link to="https://secure.acceptiva.com/?cst=gJqExh">
-          <div
-            class="p-2 bg-primary text-white mt-7 text-lg rounded mb-8 mx-10"
-          >
-            Request Card
-          </div>
-        </nuxt-link>
+        <div class="grid place-items-center">
+          <img
+            src="~/assets/mass-cards/Ring_Cross_Icon-225x300.png"
+            alt="Cross with wedding rings"
+            width="150"
+            class="mb-5"
+          />
+        </div>
+        <h2 class="text-primary text-xl font-semibold">Wedding Anniversaries</h2>
+        <a
+          href="https://secure.acceptiva.com/?cst=gJqExh"
+          target="_blank"
+          rel="nofollow noreferrer"
+        >
+          <div class="p-2 bg-primary text-white mt-7 text-lg rounded mb-8 mx-10">Request Card</div>
+        </a>
         <p class="text-left">
           The beautiful sacrament of marriage is one of the most wonderful bonds
           someone shares not only with their spouse, but with God as well. Help
@@ -60,26 +48,18 @@
           Ecclesiae Monastery.
         </p>
       </div>
-      <div
-        class="col-span-full md:col-span-4 flex flex-col m-auto text-center md:mt-0"
-        :class="{ 'mt-20': i > 0 }"
-      >
-        <img
-          src="~/assets/mass-cards/Star_Icon.png"
-          alt="Star"
-          width="150"
-          class="mb-5"
-        />
-        <h2 class="text-primary text-xl font-semibold">
-          Living Intentions
-        </h2>
-        <nuxt-link to="https://secure.acceptiva.com/?cst=9vPcNZ">
-          <div
-            class="p-2 bg-primary text-white mt-7 text-lg rounded mb-8 mx-10"
-          >
-            Request Card
-          </div>
-        </nuxt-link>
+      <div class="col-span-full md:col-span-4 flex flex-col m-auto text-center md:mt-0">
+        <div class="grid place-items-center">
+          <img src="~/assets/mass-cards/Star_Icon.png" alt="Star" width="150" class="mb-5" />
+        </div>
+        <h2 class="text-primary text-xl font-semibold">Living Intentions</h2>
+        <a
+          href="https://secure.acceptiva.com/?cst=9vPcNZ"
+          target="_blank"
+          rel="nofollow noreferrer"
+        >
+          <div class="p-2 bg-primary text-white mt-7 text-lg rounded mb-8 mx-10">Request Card</div>
+        </a>
         <p class="text-left">
           Prayers are an essential part of being Catholic – it is how we
           communicate directly with God. Bestow the gift of prayer for any
