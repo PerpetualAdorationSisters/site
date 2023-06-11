9a90a47f05d6725631476b8e0832e9eae3929013
Isaac Smith <isaac.smith.sf@gmail.com>
2023-06-11 08:16:49 -0500

    Update copyright date

--- src/site/components/Footer.vue
+++ src/site/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-auto max-w-5xl px-5 text-sm pb-7 text-gray-400">
-    © 2022 Perpetual Adoration Sisters of the Blessed Sacrament. All Rights
+    © {{ new Date().getFullYear() }} Perpetual Adoration Sisters of the Blessed Sacrament. All Rights
     Reserved.
   </div>
 </template>
